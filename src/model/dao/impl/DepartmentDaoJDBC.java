0c2188bf026d124aba664923264c7c95fc99746d
Marco Aurelio <marcoaurelio.lopes.junior@gmail.com>
2026-06-06 09:09:25 -0300

    Criaçcao de métodos de DepartmentDao e impimportação dos métodos na classe DepartmentDaoJDBC

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,8 +1,10 @@
 package model.dao.impl;
 
 import model.dao.DepartmentDao;
+import model.entities.Department;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao{
 
@@ -10,5 +12,30 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
     public DepartmentDaoJDBC(Connection conn){
         this.conn = conn;
+    }
+
+    @Override
+    public void insert(Department department) {
+
+    }
+
+    @Override
+    public void update(Department department) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Department> findAll() {
+        return List.of();
     }
 }
