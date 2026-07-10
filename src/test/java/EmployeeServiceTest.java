46ac126781286432be83728a755ab50ef9a68271
Marco Aurelio <marcoaurelio.lopes.junior@gmail.com>
2026-07-10 17:38:21 -0300

    Implementacao do teste de nome null e construcao de metodo auxiliar de criacao de employee valido

--- src/test/java/EmployeeServiceTest.java
+++ src/test/java/EmployeeServiceTest.java
@@ -1,18 +1,30 @@
 import db.DBException;
+import model.entities.Department;
 import model.entities.Employee;
+import model.entities.Position;
 import model.service.EmployeeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.sql.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EmployeeServiceTest {
 
     EmployeeService service;
+    Employee employee;
 
     @BeforeEach
     void setUp(){
         service = new EmployeeService();
+    }
+
+    private Employee createValidEmployee(){
+        Department department = new Department(1,"Teste");
+        Position position = new Position(1,"Teste");
+        Date date = new Date(2026,07,10);
+        Employee employee = new Employee(1, "Teste", 0.1, date, department, position);
+        return employee;
     }
 
     @Test
@@ -22,6 +34,15 @@ public class EmployeeServiceTest {
         String actualMessage = exception.getMessage();
         String expectedMessage = "Employee cannot be null";
         assertEquals(expectedMessage,actualMessage);
+    }
+
+    @Test
+    public void insertThrowsExceptionWithCorrectMessageWhenNameIsNull(){
+        employee = createValidEmployee();
+        employee.setName(null);
+        DBException exception = assertThrows(DBException.class, () -> service.insert(employee));
+        String expectedMessage = "Employee name cannot be null";
+        assertEquals(expectedMessage,exception.getMessage());
     }
 
     @Test
