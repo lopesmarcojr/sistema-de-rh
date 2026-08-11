7c735d18c2998ce7cf448ef6e9e4b6354b580e51
Marco Aurelio <marcoaurelio.lopes.junior@gmail.com>
2026-08-11 12:10:03 -0300

    Implementacao dos metodos findPage e count na camada service de department

--- src/main/java/model/service/DepartmentService.java
+++ src/main/java/model/service/DepartmentService.java
@@ -65,4 +65,28 @@ public class DepartmentService {
     public List<Department> findAll(){
         return departmentDao.findAll();
     }
+
+    public Integer countDepartment(){
+        return departmentDao.countDepartment();
+    }
+
+    public List<Department> findPage(int page, int pageSize){
+        validatePagination(page, pageSize);
+        return departmentDao.findPage(page, pageSize);
+    }
+
+    public void validatePagination(Integer page, Integer pageSize){
+        if(page == null){
+            throw new DBException("Page number cannot be null");
+        }
+        if(page <= 0){
+            throw new DBException("Page number should be greater than zero");
+        }
+        if(pageSize == null){
+            throw new DBException("Page size cannot be null");
+        }
+        if(pageSize <= 0){
+            throw new DBException("Page size should be greater than zero");
+        }
+    }
 }
