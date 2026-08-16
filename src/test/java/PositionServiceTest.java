94b36d5e295d135f9f0628659cb6f08b5576ce2a
Marco Aurelio <marcoaurelio.lopes.junior@gmail.com>
2026-08-16 10:17:08 -0300

    Implementacao de testes para os metodos findAll e countPositions da classe PositionService

--- src/test/java/PositionServiceTest.java
+++ src/test/java/PositionServiceTest.java
@@ -10,6 +10,9 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
@@ -99,10 +102,46 @@ public class PositionServiceTest {
 
     @ParameterizedTest
     @ValueSource(ints = {0, -1})
-    void findByIdShouldThrowExceptionWhenIdIsLessOrEqualToZero(Integer id){
+    void findByIdShouldThrowExceptionWhenIdIsLessOrEqualToZero(Integer id) {
         DBException exception = assertThrows(DBException.class, () -> service.findById(id));
         String expectedMessage = "Id should be greater than zero";
         assertEquals(expectedMessage, exception.getMessage());
         verify(positionDao, never()).findById(id);
+    }
+
+    @Test
+    void findAllShouldReturnListReturnedByPositionDao(){
+        List<Position> positions = List.of(createValidPosition());
+        when(positionDao.findAll()).thenReturn(positions);
+        List<Position> result = service.findAll();
+        assertEquals(positions, result);
+        verify(positionDao).findAll();
+    }
+
+    @Test
+    void findAllShouldReturnEmptyListWhenDaoReturnsEmptyList(){
+        List<Position> positions = Collections.emptyList();
+        when(positionDao.findAll()).thenReturn(positions);
+        List<Position> result = service.findAll();
+        assertEquals(positions, result);
+        verify(positionDao).findAll();
+    }
+
+    @Test
+    void countPositionShouldReturnCountReturnedByDao(){
+        Integer countPositions = 1;
+        when(positionDao.countPosition()).thenReturn(countPositions);
+        Integer result = service.countPosition();
+        assertEquals(countPositions, result);
+        verify(positionDao).countPosition();
+    }
+
+    @Test
+    void countPositionShouldReturnZeroWhenDaoReturnZero(){
+        Integer countPositions = 0;
+        when(positionDao.countPosition()).thenReturn(countPositions);
+        Integer result = service.countPosition();
+        assertEquals(countPositions, result);
+        verify(positionDao).countPosition();
     }
 }
